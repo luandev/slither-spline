423a698a84365189a80d5a697d1c7ddefa698b49
Luan Citta <6452989+luandev@users.noreply.github.com>
2022-03-22 19:06:57 +0000

    added eslint and added reticle fixed movement

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import * as Phaser from "phaser";
-import { CameraExample } from './scenes/CameraExample';
-import { SlitherSpline } from './scenes/SlitherSpline';
+import * as Phaser from 'phaser';
+import SlitherSpline from './scenes/SlitherSpline';
 
-var config = {
+const config = {
   type: Phaser.AUTO,
   physics: {
     default: 'arcade',
@@ -11,19 +10,13 @@ var config = {
     mode: Phaser.Scale.FIT,
     parent: 'phaser-example',
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 800,
-    height: 600
+    width: 1600,
+    height: 1200,
   },
-  backgroundColor: "#2d2d2d",
-  parent: "phaser-example",
-  scene: [SlitherSpline]
-  // scene: {
-  //   create: create,
-  //   update: update,
-  // },
+  backgroundColor: '#2d2d2d',
+  parent: 'phaser-example',
+  scene: [SlitherSpline],
 } as Phaser.Types.Core.GameConfig;
 
 const game = new Phaser.Game(config);
-
-
 export default game;
--- src/scenes/SlitherSpline.ts
+++ src/scenes/SlitherSpline.ts
@@ -1,18 +1,29 @@
 import Phaser from 'phaser';
-export class SlitherSpline extends Phaser.Scene {
+
+export default class SlitherSpline extends Phaser.Scene {
   graphics!: Phaser.GameObjects.Graphics;
+
   points: Array<[number, number]> = [];
+
   size = 30;
+
   cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
   player!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
+  reticle!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
   moveCam: boolean = true;
+
   text!: Phaser.GameObjects.Text;
+
   constructor() {
     super({
 
     });
   }
 
+  // eslint-disable-next-line class-methods-use-this
   preload() {
 
   }
@@ -32,13 +43,14 @@ export class SlitherSpline extends Phaser.Scene {
     // player = this.physics.add.image(1024, 1024, 'block');
     // player = this.physics.add.image(10, 10, 'block');
     this.player = this.physics.add.image(1024, 1024, 'block');
+    this.reticle = this.physics.add.image(800, 700, 'target');
 
     // player.setCollideWorldBounds(true);
 
-    // this.cameras.main.setZoom(0.5);
+    this.cameras.main.setZoom(1);
     // this.cameras.main.setDeadzone(400, 200);
 
-    //this.cameras.main.startFollow(this.player, true);
+    // this.cameras.main.startFollow(this.player, true);
     // this.cameras.main.startFollow(this.player, true, 0.1, 0.1);
 
     // this.cameras.main.setZoom(0.5);
@@ -49,39 +61,45 @@ export class SlitherSpline extends Phaser.Scene {
   }
 
   update() {
+    this.graphics.clear();
+    this.graphics.lineStyle(1, 0xffffff, 1);
+    this.graphics.fillStyle(0x00ff00, 1);
+
     const pointer = this.input.activePointer;
-    // const cam = this.cameras.main;
+    this.reticle.x = pointer.x;
+    this.reticle.y = pointer.y;
+    this.graphics.fillCircle(this.reticle.x, this.reticle.y, 5);
+
     const mouseBoundary = new Phaser.Geom.Circle(pointer.x, pointer.y, 80);
 
-    const head = this.points.length ? this.points[this.points.length - 1] : [0, 0];
+    const head = this.points.length
+      ? this.points[this.points.length - 1]
+      : [0, 0];
 
     const angle = Phaser.Math.Angle.Between(
       head[0],
       head[1],
       pointer.x,
-      pointer.y
+      pointer.y,
     );
 
-    const newHead = Phaser.Math.RotateTo(pointer, head[0], head[1], angle, 20);
+    const newHead = Phaser.Math.RotateTo(this.reticle, head[0], head[1], angle, 80);
     const isHeadInsideBoundary = Phaser.Geom.Circle.ContainsPoint(mouseBoundary, newHead);
 
-    if (isHeadInsideBoundary) {
-      return;
-    }
-
-    this.points.push([newHead.x, newHead.y]);
-    if (this.points.length > this.size) {
-      this.points.shift();
+    if (!isHeadInsideBoundary) {
+      this.points.push([newHead.x, newHead.y]);
+      if (this.points.length > this.size) {
+        this.points.shift();
+      }
     }
 
     this.player.x = newHead.x;
     this.player.y = newHead.y;
-    const curve = new Phaser.Curves.Spline(this.points);
+    if (this.points.length) {
+      const curve = new Phaser.Curves.Spline(this.points);
 
-    this.graphics.clear();
-    this.graphics.lineStyle(1, 0xffffff, 1);
-    this.graphics.fillStyle(0x00ff00, 1);
-    this.points.forEach(point => this.graphics.fillCircle(point[0], point[1], 4))
-    curve.draw(this.graphics, 1024);
+      this.points.forEach((point) => this.graphics.fillCircle(point[0], point[1], 5));
+      curve.draw(this.graphics, 1024);
+    }
   }
 }
